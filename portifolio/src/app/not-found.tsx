7d7bce767e7fecb997e27cdfb30dc54f1e877eb7
mateuscastronappe <teteucastro31@gmail.com>
2025-10-01 00:10:58 -0300

    criado not-found

---
+++ portifolio/src/app/not-found.tsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+    return (
+      <div className="textoNotFound w-full py-4 text-center text-gray-400">
+          <h1>404 : Not Found</h1>
+          <p>Página não encontrada!</p>
+      </div>
+    )
+  }
