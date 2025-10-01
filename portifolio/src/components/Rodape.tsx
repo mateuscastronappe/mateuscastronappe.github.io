8204c81d926d31b842fc4b17a3419c7e4fda7468
mateuscastronappe <teteucastro31@gmail.com>
2025-10-01 00:06:12 -0300

    criando o rodapé

---
+++ portifolio/src/components/Rodape.tsx
@@ -0,0 +1,8 @@
+
+export default function Rodape() {
+    return (
+<footer className="w-full py-4 text-center text-gray-400 text-sm absolute bottom-0">
+  © 2025 Mateus de Castro Nappe | Desenvolvido com <a href="https://nextjs.org" target="_blank" className="text-cyan-400 hover:underline">Next.js</a>
+</footer>
+    )
+}
