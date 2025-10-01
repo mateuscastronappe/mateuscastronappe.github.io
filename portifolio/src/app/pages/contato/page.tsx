3bd993d11d64ed3545f3962a17a0836afaf423d5
mateuscastronappe <teteucastro31@gmail.com>
2025-10-01 01:09:50 -0300

    página base de contato apenas para teste

---
+++ portifolio/src/app/pages/contato/page.tsx
@@ -0,0 +1,9 @@
+const ContatoPage = () => {
+  return (
+    <div className="min-h-screen bg-gray-900 text-white flex items-center justify-center">
+      <h1 className="text-5xl font-bold">Minha Página de Contato</h1>
+    </div>
+  );
+};
+
+export default ContatoPage;
